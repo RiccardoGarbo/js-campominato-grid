9fb48d62d61fb6255ea71546e84635e38505bf5a
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-01-10 18:43:07 +0100

    added number to cells

--- js/script.js
+++ js/script.js
@@ -8,18 +8,28 @@ const cols = 10;
 const numberOfCells = rows * cols;
 
 //FUNZIONI
-const createCell = () => {
-    const cellGenerator = `<div class="cell"></div>`
-    return cellGenerator
-
+const createCell = (content) => {
+    const cellGenerator = `<div class="cell">${content}<div>`
+    return cellGenerator;
 
 }
 //Svolgimento programma
 for (let i = 1; i <= numberOfCells; i++) {
-    const cell = createCell()
+    const cell = createCell(i)
+    console.log(cell)
     form.addEventListener('submit', (e) => {
         e.preventDefault()
         grid.innerHTML += cell
     })
 
 }
+
+
+
+
+/*const createCell = (content) => {
+    const cellGenerator = document.createElement('div')
+    cellGenerator.className = 'cell'
+    cellGenerator.innerText = content;
+    console.log(cellGenerator)
+    return cellGenerator;*/
